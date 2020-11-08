4d622b12ed19a418b19cab9bbcb6007258d9dde4
Matan Rosenberg <matan129@gmail.com>
2020-11-08 10:32:38 +0200

    Rename prompt variable

--- conf.d/999-ssh-prompt-prefix.fish
+++ conf.d/999-ssh-prompt-prefix.fish
@@ -1,9 +1,10 @@
-functions -c fish_prompt __original_prompt
+functions -e __original_prompt_for_fissh
+functions -c fish_prompt __original_prompt_for_fissh
 
 function fish_prompt --description 'Write out the prompt'
     if [ -n "$SSH_CLIENT" ]
         echo -n -s  (set_color --italics 990000) "ssh " (set_color normal)
     end
 
-    __original_prompt
+    __original_prompt_for_fissh
 end
